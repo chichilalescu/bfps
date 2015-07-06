e5e0d91e84755b8b707430edc753ab31fd409857
Cristian C Lalescu <Cristian.Lalescu@ds.mpg.de>
2015-07-06 10:22:31 +0200

    remove unused code, add clean up instruction
    when running the code, clean up previous binary files and call "make
    clean"

--- test.py
+++ test.py
@@ -28,42 +28,6 @@ import matplotlib.pyplot as plt
 import argparse
 import pickle
 
-def basic_test(
-        nsteps = 8):
-    nsteps_str = '{0}'.format(nsteps)
-    src_txt = """
-            //begincpp
-            fluid_solver<float> *fs;
-            fs = new fluid_solver<float>(32, 32, 32);
-            DEBUG_MSG("fluid_solver object created\\n");
-
-            DEBUG_MSG("nu = %g\\n", fs->nu);
-            fs->cd->read(
-                    "Kdata0",
-                    (void*)fs->cvorticity);
-            fs->low_pass_Fourier(fs->cvorticity, 3, fs->kM);
-            fs->force_divfree(fs->cvorticity);
-            fs->symmetrize(fs->cvorticity, 3);
-            DEBUG_MSG("field read\\n");
-            DEBUG_MSG(
-                "######### %d %g\\n",
-                fs->iteration,
-                fs->correl_vec(fs->cvorticity, fs->cvorticity));
-            for (int t = 0; t < """ + nsteps_str + """; t++)
-            {
-                fs->step(0.01);
-                DEBUG_MSG(
-                    "######### %d %g\\n",
-                    fs->iteration,
-                    fs->correl_vec(fs->cvorticity, fs->cvorticity));
-            }
-
-            delete fs;
-            DEBUG_MSG("fluid_solver object deleted\\n");
-            //endcpp
-            """
-    return src_txt
-
 def main(opt):
     c = convergence_test()
     c.parameters['nx'] = opt.n
@@ -74,6 +38,8 @@ def main(opt):
     c.parameters['niter_todo'] = opt.nsteps
     c.parameters['famplitude'] = 0.0
     if opt.run:
+        subprocess.call(['rm', 'test1_*', 'test2_*'])
+        subprocess.call(['make', 'clean'])
         c.execute(ncpu = opt.ncpu)
     dtype = pickle.load(open(c.name + '_dtype.pickle'))
     stats1 = np.fromfile('test1_stats.bin', dtype = dtype)
