fix tools for double precision
convergence test now works

bfps/NavierStokes.py
@@ -211,12 +211,8 @@ class NavierStokes(bfps.fluid_base.fluid_particle_base):
                 """
         return None
     def fill_up_fluid_code(self):
-        if self.dtype == np.float32:
-            C_dtype = 'float'
-        else:
-            C_dtype = 'double'
         self.fluid_includes += '#include <cstring>\n'
-        self.fluid_variables += 'fluid_solver<{0}> *fs;\n'.format(C_dtype)
+        self.fluid_variables += 'fluid_solver<{0}> *fs;\n'.format(self.C_dtype)
         self.write_fluid_stats()
         self.fluid_start += """
                 //begincpp
@@ -234,7 +230,7 @@ class NavierStokes(bfps.fluid_base.fluid_particle_base):
                 fs->iteration = iteration;
                 fs->read('v', 'c');
                 //endcpp
-                """.format(C_dtype)
+                """.format(self.C_dtype)
         self.fluid_loop += """
                 //begincpp
                 fs->step(dt);
@@ -256,15 +252,11 @@ class NavierStokes(bfps.fluid_base.fluid_particle_base):
             smoothness = 1,
             integration_steps = 2,
             kcut = 'fs->kM'):
-        if self.dtype == np.float32:
-            C_dtype = 'float'
-        else:
-            C_dtype = 'double'
         self.parameters['neighbours{0}'.format(self.particle_species)] = neighbours
         self.parameters['smoothness{0}'.format(self.particle_species)] = smoothness
         self.parameters['kcut{0}'.format(self.particle_species)] = kcut
         self.parameters['integration_steps{0}'.format(self.particle_species)] = integration_steps
-        self.particle_variables += 'tracers<{0}> *ps{1};'.format(C_dtype, self.particle_species)
+        self.particle_variables += 'tracers<{0}> *ps{1};'.format(self.C_dtype, self.particle_species)
         self.file_datasets_create += ('tmp_dspace = H5::DataSpace(2, dims, maxdims);\n' +
                                       'temp_string = std::string("/particles/") + std::string(ps{0}->name);\n' +
                                       'group = data_file.openGroup(temp_string);\n' +
@@ -305,7 +297,7 @@ class NavierStokes(bfps.fluid_base.fluid_particle_base):
                                 'ps{1}->dt = dt;\n' +
                                 'ps{1}->iteration = iteration;\n' +
                                 update_field +
-                                'ps{1}->read(&data_file);\n').format(C_dtype, self.particle_species)
+                                'ps{1}->read(&data_file);\n').format(self.C_dtype, self.particle_species)
         self.particle_loop +=  (update_field +
                                'ps{0}->step();\n' +
                                 'if (ps{0}->iteration % niter_part == 0)\n' +

bfps/fluid_converter.py
@@ -45,13 +45,9 @@ class fluid_converter(bfps.fluid_base.fluid_particle_base):
         self.finalize_code()
         return None
     def fill_up_fluid_code(self):
-        if self.dtype == np.float32:
-            C_dtype = 'float'
-        else:
-            C_dtype = 'double'
         self.fluid_includes += '#include <cstring>\n'
         self.fluid_variables += ('double t;\n' +
-                                 'fluid_solver<{0}> *fs;\n').format(C_dtype)
+                                 'fluid_solver<{0}> *fs;\n').format(self.C_dtype)
         self.fluid_definitions += """
                 //begincpp
                 void do_conversion(fluid_solver<{0}> *bla)
@@ -63,7 +59,7 @@ class fluid_converter(bfps.fluid_base.fluid_particle_base):
                         bla->write('v', 'r');
                 }}
                 //endcpp
-                """.format(C_dtype)
+                """.format(self.C_dtype)
         self.fluid_start += """
                 //begincpp
                 fs = new fluid_solver<{0}>(
@@ -73,7 +69,7 @@ class fluid_converter(bfps.fluid_base.fluid_particle_base):
                 fs->iteration = iteration;
                 do_conversion(fs);
                 //endcpp
-                """.format(C_dtype)
+                """.format(self.C_dtype)
         self.fluid_loop += """
                 //begincpp
                 fs->iteration++;