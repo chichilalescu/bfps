9037c4ff0e9d2ee48530324550ebf8af7a229fb1
Cristian C Lalescu <Cristian.Lalescu@ds.mpg.de>
2015-09-16 13:29:43 +0200

    add pure numpy integration of ABC

--- tests/test_particles.py
+++ tests/test_particles.py
@@ -53,6 +53,29 @@ def generate_ABC_flow(
     Kdata[0, 0,                    Fmode, 2] =  Famp/2.
     return Kdata
 
+def ABC_flow(x):
+    return np.array([np.cos(x[1]) + np.sin(x[2]),
+                     np.cos(x[2]) + np.sin(x[0]),
+                     np.cos(x[0]) + np.sin(x[1])])
+
+def Euler(x0, dt, nsteps, rhs):
+    x = np.zeros((nsteps+1,) + x0.shape, x0.dtype)
+    x[0] = x0
+    for i in range(nsteps):
+        x[i+1] = x[i] + dt*rhs(x[i])
+    return x
+
+def cRK(x0, dt, nsteps, rhs):
+    x = np.zeros((nsteps+1,) + x0.shape, x0.dtype)
+    x[0] = x0
+    for i in range(nsteps):
+        k1 = rhs(x[i])
+        k2 = rhs(x[i] + dt*k1*0.5)
+        k3 = rhs(x[i] + dt*k2*0.5)
+        k4 = rhs(x[i] + dt*k3)
+        x[i+1] = x[i] + dt*(k1 + 2*(k2 + k3) + k4)/6.
+    return x
+
 if __name__ == '__main__':
     opt = parser.parse_args()
     if opt.precision == 'single':
@@ -64,9 +87,38 @@ if __name__ == '__main__':
                           'ny': opt.n,
                           'nz': opt.n},
             dtype = dtype)
-    convergence_test(
+    c0, c1, c2 = convergence_test(
             opt,
             launch,
             code_class = FrozenFieldParticles,
             init_vorticity = Kdata)
+
+    xE = Euler(c0.trajectories[1][0].T,
+               c0.parameters['dt'],
+               c0.parameters['niter_todo'],
+               ABC_flow)
+    xcRK = cRK(c0.trajectories[1][0].T,
+               c0.parameters['dt'],
+               c0.parameters['niter_todo'],
+               ABC_flow)
+    fig = plt.figure(figsize=(6,6))
+    a = fig.add_subplot(111)
+    for s in range(1, c0.particle_species):
+        errlist = [np.average(np.abs(c.trajectories[s][-1, :, :3] - xcRK[-1].T))
+                   for c in [c0, c1, c2]]
+        dtlist = [c.parameters['dt']
+                  for c in [c0, c1, c2]]
+        a.plot(dtlist,
+               errlist,
+               label = '{0}'.format(s),
+               marker = '.')
+        a.plot(dtlist,
+               np.array(dtlist)**s,
+               label = '$\\Delta t^{0}$'.format(s),
+               dashes = (2, s),
+               color = (0, 0, 0))
+    a.set_xscale('log')
+    a.set_yscale('log')
+    a.legend(loc = 'best')
+    fig.savefig('test_particles_err_vs_cRK.pdf')
 
