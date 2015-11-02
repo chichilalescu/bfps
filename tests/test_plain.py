4a17ebf3b1c6042b729effc22bd9af4962ce256a
Cristian C Lalescu <Cristian.Lalescu@ds.mpg.de>
2015-11-02 17:15:16 +0100

    use bigger timestep

--- tests/test_plain.py
+++ tests/test_plain.py
@@ -36,7 +36,7 @@ parser.add_argument('--multiplejob',
 def plain(opt):
     wd = opt.work_dir
     opt.work_dir = wd + '/N{0:0>3x}_1'.format(opt.n)
-    c0 = launch(opt, dt = 0.2/opt.n)
+    c0 = launch(opt, dt = 0.4/opt.n)
     c0.compute_statistics()
     for s in range(c0.particle_species):
         acceleration_test(c0, species = s)
