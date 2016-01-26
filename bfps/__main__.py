82cf5e367faa1b20be6dd9f89ef497ae7c75bbd4
Cristian C Lalescu <Cristian.Lalescu@ds.mpg.de>
2016-01-26 21:33:07 +0100

    update comment for new .launch method

--- bfps/__main__.py
+++ bfps/__main__.py
@@ -46,7 +46,7 @@ def main():
     # first option is the choice of base class or -h or -v
     # all other options are passed on to the base_class instance
     opt = parser.parse_args(sys.argv[1:2])
-    # error is thrown if first option is not a base class, so Launcher
+    # error is thrown if first option is not a base class, so launch
     # cannot be executed by mistake.
     c = eval('{0}()'.format(opt.base_class))
     c.launch(args = sys.argv[2:])
