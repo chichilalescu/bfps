fix computation of vel gradient for sampling
I am now computing correctly the gradient of the advecting field; at
least it looks that way.
The basic sanity test is passed, histograms collapse somewhat, so I'm
happy for now.

tests/test_tensor_interpolation.py
@@ -1,37 +1,52 @@
 import sys
+import os
 import numpy as np
 from bfps import NavierStokes
 import matplotlib.pyplot as plt
 
 def main():
     c = NavierStokes()
-    #c.launch(
-    #        ['-n', '288',
-    #         '--ncpu', '4',
-    #         '--src-wd', '/home/chichi/cscratch/Lundgren_forcing/',
-    #         '--src-simname', 'N0288_kMeta2',
-    #         '--src-iteration', '4096',
-    #         '--kMeta', '2',
-    #         '--nparticles', '100000',
-    #         '--sample_gradient',
-    #         '--QR-stats',
-    #         '--niter_out', '16',
-    #         '--niter_todo', '16',
-    #         '--niter_part', '4',
-    #         '--niter_stat', '2'] +
-    #        sys.argv[1:])
+    if not os.path.exists(c.get_data_file_name()):
+        c.launch(
+            ['-n', '288',
+             '--ncpu', '4',
+             '--src-wd', '/home/chichi/cscratch/Lundgren_forcing/database',
+             '--src-simname', 'N0288_kMeta2',
+             '--src-iteration', '4096',
+             '--kMeta', '2',
+             '--nparticles', '100000',
+             '--sample_gradient',
+             '--QR-stats',
+             '--niter_out', '8',
+             '--niter_todo', '8',
+             '--niter_part', '4',
+             '--niter_stat', '2',
+             '--max_trS2_estimate', '1681'] +
+            sys.argv[1:])
     c.read_parameters()
     c.compute_statistics()
     df = c.get_data_file()
     pf = c.get_particle_file()
-    print(5*np.mean((pf['tracers0/velocity_gradient'][:, :, 0, 1] / 288**3)**2))
-    print(np.mean(df['statistics/moments/velocity_gradient'][:, 2, 0, 1]))
+    tindex = 2
+    ii = 1
+    jj = 2
+    print(np.mean((pf['tracers0/velocity_gradient'][tindex, :, ii, jj])**2))
+    print(np.mean(df['statistics/moments/velocity_gradient'][tindex, 2, ii, jj]))
+    #print(np.where(np.abs(pf['tracers0/velocity_gradient'][0, :, 0, 0]) < 1e-4)[0].shape)
     f = plt.figure()
     a = f.add_subplot(111)
     a.hist(
-            pf['tracers0/velocity_gradient'][:, :, 0, 1].ravel(),
+            pf['tracers0/velocity_gradient'][tindex, :, ii, jj],
             histtype = 'step',
-            bins = 40)
+            normed = True,
+            bins = 129)
+    bins = np.linspace(-(3*c.parameters['max_trS2_estimate'])**.5,
+                        (3*c.parameters['max_trS2_estimate'])**.5,
+                        c.parameters['histogram_bins']+1)
+    vals = .5*(bins[1:] + bins[:-1])
+    a.plot(vals,
+           df['statistics/histograms/velocity_gradient'][tindex, :, ii, jj] / (
+               c.parameters['nx']**3 * (bins[1] - bins[0])))
     a.set_yscale('log')
     f.tight_layout()
     f.savefig('uxx_PDF.pdf')