84f7637450381d15367b14528dd24ea5b272116d
Cristian C Lalescu <Cristian.Lalescu@ds.mpg.de>
2016-10-16 20:56:43 +0200

    move field_layout to own files

--- bfps/cpp/field.hpp
+++ bfps/cpp/field.hpp
@@ -28,58 +28,16 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
-#include "base.hpp"
 #include "fftw_interface.hpp"
+#include "field_layout.hpp"
 
 #ifndef FIELD_HPP
 
 #define FIELD_HPP
 
 enum field_backend {FFTW};
-enum field_components {ONE, THREE, THREExTHREE};
 enum kspace_dealias_type {TWO_THIRDS, SMOOTH};
 
-constexpr unsigned int ncomp(
-        field_components fc)
-    /* return actual number of field components for each enum value */
-{
-    return ((fc == THREE) ? 3 : (
-            (fc == THREExTHREE) ? 9 : 1));
-}
-
-constexpr unsigned int ndim(
-        field_components fc)
-    /* return actual number of field dimensions for each enum value */
-{
-    return ((fc == THREE) ? 4 : (
-            (fc == THREExTHREE) ? 5 : 3));
-}
-
-template <field_components fc>
-class field_layout
-{
-    public:
-        /* description */
-        hsize_t sizes[ndim(fc)];
-        hsize_t subsizes[ndim(fc)];
-        hsize_t starts[ndim(fc)];
-        hsize_t local_size, full_size;
-
-        int myrank, nprocs;
-        MPI_Comm comm;
-
-        std::vector<std::vector<int>> rank;
-        std::vector<std::vector<int>> all_start;
-        std::vector<std::vector<int>> all_size;
-
-        /* methods */
-        field_layout(
-                const hsize_t *SIZES,
-                const hsize_t *SUBSIZES,
-                const hsize_t *STARTS,
-                const MPI_Comm COMM_TO_USE);
-        ~field_layout(){}
-};
 
 template <field_backend be,
           kspace_dealias_type dt>
