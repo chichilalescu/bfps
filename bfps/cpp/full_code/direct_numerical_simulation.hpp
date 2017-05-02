88b440f256dd360dfff7f44802bc6e9a580cc038
Chichi Lalescu <chichilalescu@gmail.com>
2017-05-02 11:09:30 +0200

    add almost empty header

---
+++ bfps/cpp/full_code/direct_numerical_simulation.hpp
@@ -0,0 +1,45 @@
+/**********************************************************************
+*                                                                     *
+*  Copyright 2015 Max Planck Institute                                *
+*                 for Dynamics and Self-Organization                  *
+*                                                                     *
+*  This file is part of bfps.                                         *
+*                                                                     *
+*  bfps is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU General Public License as published  *
+*  by the Free Software Foundation, either version 3 of the License,  *
+*  or (at your option) any later version.                             *
+*                                                                     *
+*  bfps is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+*  GNU General Public License for more details.                       *
+*                                                                     *
+*  You should have received a copy of the GNU General Public License  *
+*  along with bfps.  If not, see <http://www.gnu.org/licenses/>       *
+*                                                                     *
+* Contact: Cristian.Lalescu@ds.mpg.de                                 *
+*                                                                     *
+**********************************************************************/
+
+
+
+#ifndef DIRECT_NUMERICAL_SIMULATION_HPP
+#define DIRECT_NUMERICAL_SIMULATION_HPP
+
+
+#include "base.hpp"
+#include "field.hpp"
+
+class direct_numerical_simulation
+{
+    public:
+        bool floating_point_exceptions;
+        int myrank, nprocs;
+        MPI_Comm comm;
+
+        std::
+};
+
+#endif//DIRECT_NUMERICAL_SIMULATION_HPP
+
