d9a9b757cc9d13b7decfac4e532549a3c082e20d
Chichi Lalescu <chichilalescu@gmail.com>
2017-05-08 15:27:34 +0200

    add child of NSVE

---
+++ bfps/cpp/full_code/NSVEparticles.cpp
@@ -0,0 +1,80 @@
+#include <string>
+#include <cmath>
+#include "NSVEparticles.hpp"
+#include "scope_timer.hpp"
+
+
+template <typename rnumber>
+int NSVEparticles<rnumber>::initialize(void)
+{
+    this->NSVE<rnumber>::initialize();
+
+    this->ps = particles_system_builder(
+                fs->cvelocity,              // (field object)
+                fs->kk,                     // (kspace object, contains dkx, dky, dkz)
+                tracers0_integration_steps, // to check coherency between parameters and hdf input file (nb rhs)
+                (long long int)nparticles,  // to check coherency between parameters and hdf input file
+                fs->get_current_fname(),    // particles input filename
+                std::string("/tracers0/state/") + std::to_string(fs->iteration), // dataset name for initial input
+                std::string("/tracers0/rhs/")  + std::to_string(fs->iteration), // dataset name for initial input
+                tracers0_neighbours,        // parameter (interpolation no neighbours)
+                tracers0_smoothness,        // parameter
+                this->comm,
+                fs->iteration+1);
+    this->particles_output_writer_mpi = new particles_output_hdf5<long long int,double,3,3>(
+                MPI_COMM_WORLD,
+                "tracers0",
+                nparticles,
+                tracers0_integration_steps);
+    return EXIT_SUCCESS;
+}
+
+template <typename rnumber>
+int NSVEparticles<rnumber>::step(void)
+{
+    this->fs->compute_velocity(fs->cvorticity);
+    this->fs->cvelocity->ift();
+    this->ps->completeLoop(this->dt);
+    this->NSVE<rnumber>::step();
+    return EXIT_SUCCESS;
+}
+
+template <typename rnumber>
+int NSVEparticles<rnumber>::write_checkpoint(void)
+{
+    this->NSVE<rnumber>::write_checkpoint();
+    this->particles_output_writer_mpi->open_file(fs->get_current_fname());
+    this->particles_output_writer_mpi->save(
+            this->ps->getParticlesPositions(),
+            this->ps->getParticlesRhs(),
+            this->ps->getParticlesIndexes(),
+            this->ps->getLocalNbParticles(),
+            this->fs->iteration);
+    this->particles_output_writer_mpi->close_file();
+    return EXIT_SUCCESS;
+}
+
+template <typename rnumber>
+int NSVEparticles<rnumber>::finalize(void)
+{
+    this->NSVE<rnumber>::finalize();
+    this->ps.release();
+    delete this->particles_output_writer_mpi;
+    return EXIT_SUCCESS;
+}
+
+template <typename rnumber>
+int NSVEparticles<rnumber>::do_stats()
+{
+    // fluid stats go here
+    this->NSVE<rnumber>::do_stats();
+    // particle sampling should go here
+    //if (this->iteration % this->niter_part == 0)
+    //{
+    //}
+    return EXIT_SUCCESS;
+}
+
+template class NSVEparticles<float>;
+template class NSVEparticles<double>;
+
