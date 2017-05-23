1aa6365076e3039cb197cf8d090b8dfc0e6d65e5
Berenger Bramas <bbramas@mpcdf.mpg.de>
2017-05-23 11:06:38 +0200

    update ci (change install dir) -- update test command

--- tests/ci-scripts/test.sh
+++ tests/ci-scripts/test.sh
@@ -6,12 +6,12 @@ set -x
 set -e
 
 # Init
-source ~/.bashrc
-export destdir=/tmp/bfpsinstall
+export destdir=$(pwd)"/ci-installdir"
 export pythonbin=/home/ubuntu/anaconda3/bin/python3
 export bfpspythonpath=$destdir/lib/python3.6/site-packages/
 export PYTHONPATH=:$bfpspythonpath$PYTHONPATH
-export PATH=$destdir/bin/:$PATH
+export PATH=$destdir/bin/:/home/ubuntu/hdf5/install/bin/:$PATH
+export LD_LIBRARY_PATH=/home/ubuntu/hdf5/install/lib/:/home/ubuntu/fftw/install/lib/
 
 echo "destdir = $destdir"
 echo "pythonbin = $pythonbin"
@@ -36,7 +36,7 @@ $pythonbin setup.py install --prefix=$destdir
 ls $destdir
 ls $destdir/bin/
 
-$pythonbin $destdir/bin/test_bfps_NSVEparticles.py
+$pythonbin $destdir/bin/bfps.test_NSVEparticles
 
 # Clean
 if [[ -d $destdir ]] ; then
